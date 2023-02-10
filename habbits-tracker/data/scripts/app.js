f2def2fc276793f9014337b4a8c533c689d22cca
margosha98 <75506944+margosha98@users.noreply.github.com>
2023-02-10 23:59:58 +0200

    Add files via upload

---
+++ habbits-tracker/data/scripts/app.js
@@ -0,0 +1,234 @@
+// 'use strict'
+
+let habbits = []
+const HABBITS_KEY = "HABBITS_KEY"
+let globalHabbitId
+
+let page = {
+    menu: document.querySelector('.menu__list'),
+    header: {
+        title: document.querySelector('.title'),
+        progressPersent: document.querySelector('.progress__percent'),
+        progressCoverBar: document.querySelector('.progress__cover-bar'),
+    },
+    content: {
+        daysContainer: document.querySelector('.days_container'),
+        newDay: document.querySelector('.habbit__day')
+    },
+    popup: {
+        window: document.querySelector('.case'),
+        popupInputIcon: document.querySelector('input[name="popup-icon_input"]'),
+        popupInputTarget: document.querySelector('input[name="habbit-target"]'),
+        popupInputTitle:  document.querySelector('input[name="habbit-title"]')
+    }
+}
+/* utils */
+
+function loadData() {
+    const stringData = localStorage.getItem(HABBITS_KEY)
+    const arrayData = JSON.parse(stringData)
+
+    if (Array.isArray(arrayData)) {
+        habbits = arrayData
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBITS_KEY, JSON.stringify(habbits))
+}
+
+
+/* render */
+
+function rerenderMenu(activeHabbit) {
+    page.content.innerHTML = ''
+    for (let habbit of habbits) {
+        const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`)
+        if (!existed) {
+            const el = document.createElement("button")
+            el.setAttribute('menu-habbit-id', habbit.id)
+            el.classList.add('menu__item')
+            el.addEventListener('click', () => rerender(habbit.id))
+            el.innerHTML = `<img src="./src/${habbit.icon}.svg" alt=${habbit.name}>`
+
+            if (habbit.id === activeHabbit.id) {
+                el.classList.add('menu__item_active')
+            }
+            page.menu.appendChild(el)
+
+            continue
+        }
+        if (activeHabbit.id === habbit.id) {
+            existed.classList.add('menu__item_active')
+        } else {
+            existed.classList.remove('menu__item_active')
+        }
+
+    }
+
+}
+
+function rerenderHeader(activeHabbit) {
+    page.header.title.innerText = activeHabbit.name
+
+    let progress = activeHabbit.days.length / activeHabbit.target * 100
+    let persentProgress = progress >= 100 ? 100 : progress.toFixed(0)
+    page.header.progressPersent.innerText = `${persentProgress}%`
+    page.header.progressCoverBar.style.width = `${persentProgress}%`
+
+}
+
+function renderContent(activeHabbit) {
+    if (habbits.length!==0) {
+        document.querySelector('.start_display').classList.add('.hiden')
+        document.querySelector('main').classList.remove('.hiden')
+    } else {
+        document.querySelector('.start_display').classList.remove('.hiden')
+        document.querySelector('.start_display').classList.add('.hiden')
+    }
+    let days = activeHabbit.days
+    page.content.daysContainer.innerHTML = ''
+
+    for (let day in days) {
+
+        let dayEl = document.createElement("div")
+        dayEl.classList.add("habbit")
+        dayEl.innerHTML = `
+            <div class="habbit__day">День ${Number(day)+1}</div>
+            <div class="habbit__comment">${days[Number(day)].comment}</div>
+            <button class="habbit__delete" onClick="deleteDay(${Number(day)})">
+              <img src="./src/delete.svg" />
+            </button>
+        `
+        page.content.daysContainer.appendChild(dayEl)
+    }
+    page.content.newDay.innerText = `День ${days.length+1}`
+
+}
+
+
+function rerender(activeHabbitId) {
+    // document.querySelector('[name=writedComment]').classList.remove('error')
+    globalHabbitId = activeHabbitId
+    document.querySelector('.start_display').classList.add('hiden')
+    document.querySelector('main').classList.remove('hiden')
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId)
+    if (!activeHabbit) {
+        return
+    }
+
+    rerenderMenu(activeHabbit)
+    rerenderHeader(activeHabbit)
+    renderContent(activeHabbit)
+}
+
+/* work with comment form */
+function addNewDay(event) {
+    const form = event.target
+    event.preventDefault()
+    form['writedComment'].classList.remove('error')
+    let data = new FormData(event.target);
+    comment = data.get("writedComment")
+    if (!comment) {
+        console.log('comment')
+        form['writedComment'].classList.add('error')
+    } else {
+        habbits = habbits.map(habbit => {
+            if (habbit.id === globalHabbitId) {
+                return {
+                    ...habbit,
+                    days: [...habbit.days, {
+                        comment
+                    }]
+                }
+            }
+            return habbit
+        })
+        form['writedComment'].value = ''
+    }
+    saveData()
+    rerender(globalHabbitId)
+}
+
+function deleteDay(indexEl) {
+    debugger
+    habbits[globalHabbitId].days.splice(indexEl,1)
+    debugger
+    console.log(habbits)
+    saveData()
+    rerender(globalHabbitId)
+}
+
+function togglePopup(bool) {
+    if (document.querySelector('.warningEl')) {
+        document.querySelector('.popup__input_container').removeChild(document.querySelector('.warningEl'))
+    }
+    
+    if (bool) {
+        page.popup.window.classList.add('open')
+    } else {
+        page.popup.window.classList.remove('open')
+    }
+}
+
+/* work with popap */
+
+function resetData(el,array) {
+    array.forEach(inputName => {
+        el.querySelector(`[name=${inputName}]`).value = ''
+    });
+}
+
+function setPopupIcon(context, icon) {
+    page.popup.window.querySelector('.menu__item_active').classList.remove('menu__item_active')
+    context.classList.add('menu__item_active')
+
+    page.popup.popupInputIcon.value = icon
+
+}
+
+function getpopupInputValue(name) {
+    return document.querySelector(`[name=${name}]`).value
+}
+
+function addNewHabbitItem(event) {
+    let popupInputIconValue = page.popup.popupInputIcon.value
+    let popupInputTitleValue = page.popup.popupInputTitle.value
+    let popapInputTargetValue =  page.popup.popupInputTarget.value
+
+    resetData(event.target, ['habbit-title','habbit-target'])
+
+    event.preventDefault()
+    if (popupInputIconValue && popupInputTitleValue && Number(popapInputTargetValue)) {
+        let newHabbitItem = {
+            "id": habbits.length,
+            "icon": popupInputIconValue,
+            "name": popupInputTitleValue,
+            "target": popapInputTargetValue,
+            "days": []
+        }
+        habbits.push(newHabbitItem)
+        saveData()
+        rerender(newHabbitItem.id)
+        page.popup.window.classList.remove('open')
+    } else {
+        if (!document.querySelector('.warningEl')) {
+            let warningEl = document.createElement('p')
+            warningEl.classList.add('warningEl')
+            warningEl.innerText = 'Не заполнены или некорректно заполнены элементы ввода!'
+            document.querySelector('.popup__input_container').appendChild(warningEl)
+        }
+    }
+}
+
+/* init */
+
+(() => {
+    loadData();
+    if (habbits.length === 0 ) {
+            document.querySelector('.start_display').classList.remove('hiden')
+            document.querySelector('main').classList.add('hiden')
+    } else {
+    rerender(habbits[0].id)
+    }
+})()
